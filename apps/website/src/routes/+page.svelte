0c59930399c1c5b921679eea34a0a2e8ce17e5e0
Lukas Stracke <lukas.stracke@sentry.io>
2023-09-24 00:27:24 +0200

    initial site

--- apps/website/src/routes/+page.svelte
+++ apps/website/src/routes/+page.svelte
@@ -1,1 +1,16 @@
-<h1>Open Sky Mods</h1>
+<script lang="ts">
+  import IconCloud from './IconCloud.svelte';
+</script>
+
+<div class="h-screen flex flex-col items-center justify-center gap-4">
+  <h1 class="text-xl inline-flex gap-2 items-center">
+    <IconCloud />Open Sky Mods
+  </h1>
+
+  <p class="max-w-md text-gray-400">
+    Imagine a world where you can manage all your GSX profiles in one place. Simple 1-Click download
+    and install any profile. Open Sky Mods will do this.
+  </p>
+  <p class="text-gray-400">All open, all free, always.</p>
+  <p class="text-gray-400">Coming soon...</p>
+</div>
